13c34ff0449433c16f0dabd32b3b21f80a295091
Athif718 <athife71@gmail.com>
2026-02-19 11:52:10 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import book
+# from django.http import HttpResponse
+# Create your views here.
+
+
+# def home(request):
+#     data=["Aplle","Orange","Mango"]
+#     return render(request,"abc.html",{"abc":data})
+
+# def new(request):
+#     return render(request,"new.html")
+
+# def about(request):
+#     return render(request,"about.html")
